0227ebc64c2257d9d31e51d8e1eb33b397da8ad4
TalitaJames <97495939+TalitaJames@users.noreply.github.com>
2022-03-11 10:44:16 +1100

    Still wrong Day1

--- 2021/day 01/part1.py
+++ 2021/day 01/part1.py
@@ -9,14 +9,13 @@ def solve(data):
 
     #FIXME: Prints as 1389, but the answer is 1390, logic error somewhere in the loop
     for i in range(1,len(data)):
-        
         if data[i] > data[i-1]:
             sum+=1
 
-    
  
     return sum
 
 
 
 print(solve(parse('input.txt')))
+print("------------------------ Solution")
