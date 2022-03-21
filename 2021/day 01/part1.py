0e0ce5a00e1dfed3b514893e33033db9f5d04b2f
TalitaJames <97495939+TalitaJames@users.noreply.github.com>
2022-03-21 13:05:31 +1100

    Update part1.py

--- 2021/day 01/part1.py
+++ 2021/day 01/part1.py
@@ -8,8 +8,8 @@ def solve(data):
     sum = 0
 
     #FIXME: Prints as 1389, but the answer is 1390, logic error somewhere in the loop
-    for i in range(1,len(data)):
-        if data[i] > data[i-1]:
+    for i in range(0,len(data)-1):
+        if data[i] < data[i+1]:
             sum+=1
 
  
